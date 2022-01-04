00d010a1a55706d2f3233ac5edb655ee9b6075ec
Viviana Pentangelo <pentangeloviviana@gmail.com>
2022-01-04 15:33:51 +0100

    Implementazione pezzo fantasma

--- Tetris 2D/Assets/Scripts/Ghost.cs
+++ Tetris 2D/Assets/Scripts/Ghost.cs
@@ -1,18 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Ghost : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Tile tile;
+    public Board board;
+    public Piece trackingPiece;
+    public Tilemap tilemap { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
+    private void Awake()
     {
-        
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.cells = new Vector3Int[4];
+
     }
 
-    // Update is called once per frame
-    void Update()
+    //Funzione di Update chiamata dopo tutte le altre funzioni di Update
+    private void LateUpdate()
     {
-        
+        Clear();
+        Copy();
+        Drop();
+        Set();
     }
+
+    private void Clear()
+    {
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3Int tilePosition = this.cells[i] + this.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    //Il pezzo fantasma copia la forma del pezzo attualmente attivo
+    private void Copy()
+    {
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            this.cells[i] = this.trackingPiece.cells[i];
+        }
+    }
+
+    private void Drop()
+    {
+        Vector3Int position = this.trackingPiece.position;
+        int current = position.y;
+        int bottom = -this.board.boardSize.y / 2 - 1;
+
+        this.board.Clear(this.trackingPiece);
+
+        for(int row = current; row >= bottom; row--) {
+            position.y = row;
+
+            if(this.board.IsValidPosition(this.trackingPiece, position)) {
+                this.position = position;
+            }
+            else { break; }
+        }
+
+        this.board.Set(this.trackingPiece);
+    }
+
+    private void Set()
+    {
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+
+            //Vengono prese le singole celle e shiftate della posizione totale del pezzo
+            Vector3Int tilePosition = this.cells[i] + this.position;
+
+            //Vengono disegnati i tile sulla mappa
+            this.tilemap.SetTile(tilePosition, this.tile);
+        }
+    }
+
+
 }
